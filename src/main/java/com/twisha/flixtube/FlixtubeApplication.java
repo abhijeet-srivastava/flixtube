451bf9f58b5dbdaef173706eb48734cd344766d1
abhijeet.srivastva <malviyanabhijeet@gmail.com>
2025-04-14 12:55:14 +0530

    Adding first commit

---
+++ src/main/java/com/twisha/flixtube/FlixtubeApplication.java
@@ -0,0 +1,13 @@
+package com.twisha.flixtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlixtubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlixtubeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/twisha/flixtube/FlixtubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twisha.flixtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlixtubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
