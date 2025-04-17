b7cbbd1fd42d7ad283a6d390ec0fccb95413b3b2
abhijeet.srivastva <malviyanabhijeet@gmail.com>
2025-04-17 10:52:32 +0530

    Adding tests

--- src/test/java/com/twisha/flixtube/FlixtubeApplicationTests.java
+++ src/test/java/com/twisha/flixtube/FlixtubeApplicationTests.java
@@ -1,10 +1,15 @@
 package com.twisha.flixtube;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FlixtubeApplicationTests {
+
+    @Autowired
+    private TestRestTemplate template;
 
     @Test
     void contextLoads() {
